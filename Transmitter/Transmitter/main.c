1860e7ca16660c24fc68068795403fe746161bc7
mrkefskie <mrkefskie@gmail.com>
2016-06-14 09:59:08 +0200

    Changed folders

---
+++ Transmitter/Transmitter/main.c
@@ -0,0 +1,18 @@
+/*
+ * Transmitter.c
+ *
+ * Created: 14-6-2016 09:55:51
+ * Author : Kevin Slagmolen
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
